79323ce991d2222c0f92973d9d6b1af5f558bdef
Jocker666z <r.barbarot@gmail.com>
2020-05-24 11:07:20 +0200

    v0.01

---
+++ README.md
@@ -0,0 +1,8 @@
+## Vigilance Meteo France par departement
+Script bash/python simple et un peu sale, il n'affiche rien si il n'y a pas de vigilance. Ci-dessous un exemple.
+```
+bash vigilance-meteo.sh 38
+🟨 Inondation
+```
+## Dependences
+`pip install vigilancemeteo`
---
+++ vigilance-meteo.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Affiche les vigilances de meteo france par departement
+#
+# Author : Romain Barbarot
+# https://github.com/Jocker666z/
+#
+# licence : GNU GPL-2.0
+# Dependance: vigilancemeteo (MIT) https://github.com/oncleben31/vigilancemeteo
+
+
+# Argument
+if [[ "$1" =~ ^-?[0-9]+$ ]]; then
+	DEPARTEMENT="$1"
+else
+	echo "Usage: vigilance-meteo.sh \"Numero du departement\""
+	exit
+fi
+
+VigilanceImport() {
+python3 << END
+import vigilancemeteo
+zone = vigilancemeteo.DepartmentWeatherAlert("$DEPARTEMENT")
+zone.department_color
+zone.additional_info_URL
+print(zone.summary_message('text'))
+END
+}
+
+Vigilance() {
+mapfile -t RawVigilanceList < <(VigilanceImport | sed '1d')
+NBV="${#RawVigilanceList[@]}"
+if [ "$NBV" -gt "0" ]; then
+	for i in "${RawVigilanceList[@]}"; do
+		i=$(echo "$i" | sed 's/^...//')			# Remove 3 first characters of all lines
+		if [[ $i == *"Jaune"* ]]; then
+			i=$(echo "$i" | sed 's/^/🟨 /')		# Add color icon at start of line
+		fi
+		if [[ $i == *"Orange"* ]]; then
+			i=$(echo "$i" | sed 's/^/🟧 /')
+		fi
+		if [[ $i == *"Rouge"* ]]; then
+			i=$(echo "$i" | sed 's/^/🟥 /')
+		fi
+		i=$(echo "$i" | cut -f1 -d":")			# Remove all after ":"
+		echo "$i"
+	done
+fi
+}
+
+Vigilance
+
+exit
